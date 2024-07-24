cf61bef56d22dac72817815bd48c2b841049b2a5
adel boullif <adoula136@gmail.com>
2024-07-25 00:16:50 +0200

    gdgdg

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,83 +1,3 @@
-// import React from "react"
-// import { useState } from "react"
-// import { FaClock } from "react-icons/fa"
-// import Skeleton, { SkeletonTheme } from "react-loading-skeleton"
-// import "react-loading-skeleton/dist/skeleton.css"
-
-// const PostCard = React.forwardRef(
-//   (
-//     {
-//       title = "Titre du poste",
-//       photo,
-//       date = null,
-//       onClick,
-//       liked = false,
-//       isHome = false,
-//     },
-//     ref
-//   ) => {
-//     const [hover, setHover] = useState(false)
-//     const [imageLoaded, setImageLoaded] = useState(false)
-
-//     return (
-//       <div
-//         ref={ref}
-//         onClick={onClick}
-//         onMouseEnter={() => setHover(true)}
-//         onMouseLeave={() => setHover(false)}
-//         className={`flex flex-col
-//         ${isHome && "bg-black"}
-//         z-10 items-center relative my-3 overflow-hidden pb-3 rounded-lg shadow-md mx-2 gap-3 lg:max-w-lg max-w-sm cursor-pointer `}
-//       >
-//         {imageLoaded ? (
-//           <img
-//             src={photo}
-//             alt={title}
-//             onLoad={() => setImageLoaded(true)}
-//             className="lg:h-[14rem] h-[12rem] lg:w-[20rem] w-[19rem] object-fill"
-//           />
-//         ) : (
-//           <SkeletonTheme baseColor="#202020" highlightColor="#444">
-//             <Skeleton height={250} width={"20rem"} className="my-2" />
-//           </SkeletonTheme>
-//         )}
-//         {!imageLoaded && (
-//           <img
-//             src={photo}
-//             alt={title}
-//             style={{ display: "none" }}
-//             onLoad={() => setImageLoaded(true)}
-//           />
-//         )}
-//         <h3
-//           className={`font-semibold ${
-//             isHome && "text-yellow-300"
-//           }  truncate lg:max-w-[17rem] max-w-[16rem]`}
-//         >
-//           {title}
-//         </h3>
-
-//         {date && (
-//           <div className="flex items-center">
-//             <FaClock className={`${isHome && "text-yellow-300"}`} />
-//             <p className="ml-1 text-sm text-gray-400">{date}</p>
-//           </div>
-//         )}
-
-//         {hover && (
-//           <div className="absolute bottom-0 w-full z-10">
-//             <button className="bg-yellow-300 w-full lg:py-1 ">
-//               Voir plus d&apos;infos
-//             </button>
-//           </div>
-//         )}
-//       </div>
-//     )
-//   }
-// )
-
-// export default PostCard
-
 import React from "react"
 import { useState } from "react"
 import { FaClock } from "react-icons/fa"
@@ -87,7 +7,7 @@ import "react-loading-skeleton/dist/skeleton.css"
 const PostCard = React.forwardRef(
   (
     {
-      title = "Titre du poste",
+      title = "sorties, restaurents , hotels , en algérie",
       photo,
       date = null,
       onClick,
@@ -133,13 +53,13 @@ const PostCard = React.forwardRef(
             onLoad={() => setImageLoaded(true)}
           />
         )}
-        <h3
+        <h2
           className={`font-semibold ${
             isHome && "text-yellow-300"
           }  truncate lg:max-w-[17rem] max-w-[16rem]`}
         >
           {title}
-        </h3>
+        </h2>
 
         {date && isValidDate(new Date(date)) && (
           <div className="flex items-center">
